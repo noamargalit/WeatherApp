22c8dfb09df962352929203fea7be93522591426
Margalit Noa <margalitnoa@Margalits-MacBook-Pro.local>
2019-01-23 15:40:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/margalitnoa/weatherapp/Common/Common.java
@@ -0,0 +1,36 @@
+package com.example.margalitnoa.weatherapp.Common;
+
+import android.support.annotation.NonNull;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Common {
+    public static String API_KEY = "9b11c7a551f1f45c0f2651ad3c71e312";
+    public static String API_LINK = "http://api.openweathermap.org/data/2.5/weather";
+
+    @NonNull
+    public static String apiRequest(String lat, String lng){
+        StringBuilder sb = new StringBuilder(API_LINK);
+        sb.append(String.format("?lat =%s&lon=%s$APPID=%s&units=metric",lat,lng,API_KEY));
+        return sb.toString();
+    }
+
+    public static String unixTimeStampToDateTime(double unixTimeStamp){
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        date.setTime((long)(unixTimeStamp*1000));
+        return dateFormat.format(date);
+    }
+
+    public static String getImage(String icon){
+        return String.format("http://openweathermap.org/img/w/%s.png",icon);
+    }
+
+    public static String getDateNow(){
+        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy HH:mm");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+}
---
+++ app/src/main/java/com/example/margalitnoa/weatherapp/Model/Rain.java
@@ -0,0 +1,5 @@
+package com.example.margalitnoa.weatherapp.Model;
+
+public class Rain {
+
+}
